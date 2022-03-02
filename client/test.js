96505996fb3aaecf88756769c3b6945a85cac778
jhwook <sprtzk@naver.com>
2022-03-02 21:02:07 +0900

    Add: 초기 세팅 파일 업로드

---
+++ client/test.js
@@ -0,0 +1,6 @@
+import express from "express";
+
+const func = function () {
+  const foo = "asfasdf";
+  return foo;
+};
---
+++ server/test.js
@@ -0,0 +1,5 @@
+import express from "express";
+let func = function () {
+  let foo = "aaaaaaa";
+  return foo;
+};
