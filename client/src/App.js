6898cb2e0f119b63808270dd42e4088f53086c1c
kimdaeyoon <eodbszla@gmail.com>
2022-03-04 11:46:45 +0900

    FIX: 프론트엔드 파일 전면 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
 function App() {
-  return null;
+  return (
+    <div>
+      <div>Hello World</div>
+      <div>Hello World</div>
+      <div>Hello World</div>
+      <div>Hello World</div>
+    </div>
+  );
 }
 
 export default App;
