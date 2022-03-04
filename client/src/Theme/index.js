d1cbe53ff767d4bb69845c8ce1b3a568f24db395
kimdaeyoon <eodbszla@gmail.com>
2022-03-04 13:25:13 +0900

    Fix: 프론트엔드 세팅 파일 전면 수정

---
+++ client/src/Theme/index.js
@@ -0,0 +1,16 @@
+export const lightTheme = {
+  bgColor: {
+    green: "#9ccc65",
+    white: "white",
+    black: "black",
+    skyblue: "skyblue",
+  },
+  textColor: "black",
+  fontSize: {
+    tiny: "18px",
+    small: "23px",
+    medium: "35px",
+    large: "45px",
+    huge: "60px",
+  },
+};
